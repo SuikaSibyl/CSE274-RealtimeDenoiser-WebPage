d34b905cb4d099714139bb461401937e1d3147e8
Suika <60009633+SuikaSibyl@users.noreply.github.com>
2022-11-09 21:52:10 -0800

    Use before-after slider

--- cse274/src/views/HomeView.vue
+++ cse274/src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <div id="mySlider"></div>
+    <div id="whatever">hello</div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -8,11 +10,25 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import SliderBar from 'before-after-slider'; // import
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld,
+  },
+  mounted() {
+    // new SliderBar({options});
+    /* eslint-disable no-new */
+    new SliderBar({
+      el: '#mySlider',
+      beforeImg: 'https://github.com/SuikaSibyl/MyFigures/blob/main/ImageHost/274denoiser-filtered-gi-cornellbox.png',
+      afterImg: 'https://github.com/SuikaSibyl/MyFigures/blob/main/ImageHost/274denoiser-benchmark-gi-cornellbox.png',
+      width: '90%', // slide-wrap width, default 100%
+      height: '400px', // slide-wrap height, default image-height
+      line: false, // Dividing line, default true
+      lineColor: 'rgba(0,0,0,0.5)', // Dividing line color, default rgba(0,0,0,0.5)
+    });
   },
 };
 </script>
