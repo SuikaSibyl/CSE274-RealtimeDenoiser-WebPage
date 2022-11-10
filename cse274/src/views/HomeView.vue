2384dd63891dc7e2f345955ccd07e3434a040559
Suika <60009633+SuikaSibyl@users.noreply.github.com>
2022-11-09 22:54:03 -0800

    Enable Image Access

--- cse274/src/views/HomeView.vue
+++ cse274/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <div id="mySlider"></div>
+    <div id="giUnfiltFilt"></div>
+    <div id="giFiltGT"></div>
     <div id="whatever">hello</div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -21,13 +22,22 @@ export default {
     // new SliderBar({options});
     /* eslint-disable no-new */
     new SliderBar({
-      el: '#mySlider',
-      beforeImg: 'https://github.com/SuikaSibyl/MyFigures/blob/main/ImageHost/274denoiser-filtered-gi-cornellbox.png',
-      afterImg: 'https://github.com/SuikaSibyl/MyFigures/blob/main/ImageHost/274denoiser-benchmark-gi-cornellbox.png',
+      el: '#giUnfiltFilt',
+      beforeImg: 'https://imagehost-suikasibyl-us.oss-us-west-1.aliyuncs.com/274denoiser-noisy-gi-cornellbox.png',
+      afterImg: 'https://imagehost-suikasibyl-us.oss-us-west-1.aliyuncs.com/274denoiser-filtered-gi-cornellbox.png',
       width: '90%', // slide-wrap width, default 100%
       height: '400px', // slide-wrap height, default image-height
-      line: false, // Dividing line, default true
-      lineColor: 'rgba(0,0,0,0.5)', // Dividing line color, default rgba(0,0,0,0.5)
+      line: true, // Dividing line, default true
+      lineColor: 'rgba(0,0,0,0.1)', // Dividing line color, default rgba(0,0,0,0.5)
+    });
+    new SliderBar({
+      el: '#giFiltGT',
+      beforeImg: 'https://imagehost-suikasibyl-us.oss-us-west-1.aliyuncs.com/274denoiser-filtered-gi-cornellbox.png',
+      afterImg: 'https://imagehost-suikasibyl-us.oss-us-west-1.aliyuncs.com/274denoiser-benchmark-gi-cornellbox.png',
+      width: '90%', // slide-wrap width, default 100%
+      height: '400px', // slide-wrap height, default image-height
+      line: true, // Dividing line, default true
+      lineColor: 'rgba(0,0,0,0.1)', // Dividing line color, default rgba(0,0,0,0.5)
     });
   },
 };
